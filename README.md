3a4188f70d724da33cf822326c11ed351bb5d756
peter <better_blood@hotmail.com>
2023-10-07 16:06:26 +0200

    Merge branch 'main' into 10-picocli

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# WIP
+## Description 
+
+This CLI tool is used to convert webp files to png/jpg and vice versa
+
+## Usage
+
+Use ```mvn package``` to build the application.
+
+In the terminal, use ```java -jar target/dai-pw1-1.0-SNAPSHOT.jar``` to run the application.
--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -45,33 +45,7 @@ public class Main implements Callable<Integer> {
 
     @Option(names={"-e", "--echo"}, description = "echo ${DEFAULT-VALUE}", required = false, defaultValue = "test")
     private String echoValue;
-
-    private static void decode() throws IOException {
-        // Obtain a WebP ImageReader instance
-        ImageReader reader = ImageIO.getImageReadersByMIMEType("image/webp").next();
-        // Configure decoding parameters
-        WebPReadParam readParam = new WebPReadParam();
-        readParam.setBypassFiltering(true);
-        // Configure the input on the ImageReader
-        reader.setInput(new FileImageInputStream(new File("input.webp")));
-        // Decode the image
-        BufferedImage image = reader.read(0, readParam);
-    }
-
-    private static void encode() throws IOException {
-        // Obtain an image to encode from somewhere
-        BufferedImage image = ImageIO.read(new File("input.png"));
-        // Obtain a WebP ImageWriter instance
-        ImageWriter writer = ImageIO.getImageWritersByMIMEType("image/webp").next();
-        // Configure encoding parameters
-        WebPWriteParam writeParam = new WebPWriteParam(writer.getLocale());
-        writeParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        writeParam.setCompressionType(writeParam.getCompressionTypes()[WebPWriteParam.LOSSLESS_COMPRESSION]);        // Configure the output on the ImageWriter
-        writer.setOutput(new FileImageOutputStream(new File("output.webp")));
-        // Encode
-        writer.write(null, new IIOImage(image, null, null), writeParam);
-    }
-
+    
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
